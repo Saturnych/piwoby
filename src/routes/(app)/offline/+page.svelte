7248c998c1cf6dd5c2bda5defc8eda9c199acd49
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-04-13 16:06:16 +0300

    prerender configs

--- src/routes/(app)/offline/+page.svelte
+++ src/routes/(app)/offline/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import Head from '$lib/components/layout/Head.svelte';
+</script>
+
+<Head />
+
 <div class="pt-64 mx-auto w-64 text-center">
 	<h1 class="text-9xl font-bold text-outline">Offline</h1>
 	<p class="text-2xl font-semibold">The page you have requested is offline</p>
