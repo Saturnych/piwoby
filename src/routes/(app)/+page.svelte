e5417d6f1a2b51a29124732fde729cc23138d611
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-04-17 00:08:59 +0300

    Update +page.svelte

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import Head from '$lib/components/layout/Head.svelte';
 	import Blogs from '$lib/components/Blogs.svelte';
+	import { config } from '$lib/config';
 
 	export let data;
 	const { posts = [] } = data;
 </script>
 
-<Head />
+<Head title="{config?.headerTitle}" />
 
 <div class="pt-12">
 	<h1 class="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">Первый белорусский портал про пиво</h1>
