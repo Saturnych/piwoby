f36d507186830e004e19c7c3a1eac04dc3f53de4
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-12-16 10:50:34 +0300

    updated

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,39 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)]
 	kit: {
-		adapter: adapter(),
+		appDir: 'app', // Required as the default is _app
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		},
+		adapter: adapter({
+			fallback: 'index.html' // may differ from host to host
+		}),
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
+				/*
 				// ignore deliberate link to shiny 404 page
-				if (path === '/404' && referrer.startsWith('/blog')) {
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
 					return;
 				}
+				*/
+
 				// otherwise fail the build
-				//throw new Error(message);
-				//return;
+				throw new Error(message);
 			}
 		},
-		// remove this if you're not using comment system
-		//csp: { mode: 'auto' }
-	},
-	preprocess: [mdsvex(mdsvexConfig), vitePreprocess()]
+		csrf: { checkOrigin: false }
+	}
 };
 
 export default config;
